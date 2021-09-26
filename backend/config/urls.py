ec0460a5f0b7933632d031459d452536e356cc6b
Kuchmenko-Pavlo <sigmakmn@gmail.com>
2021-09-26 08:18:13 +0300

    Server_3

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from posts.views import PostsListView
+from posts.views import PostsListView, PostDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', PostsListView.as_view()),
+    path('posts/<int:pk>/', PostDetailView.as_view()),
 ]
+urlpatterns += staticfiles_urlpatterns()
+
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,9 +1,19 @@
 from posts.models import Post
 from django.http import HttpResponse, HttpRequest
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
 class PostsListView(ListView):
     model = Post
     template_name = 'posts/index.html'
+
+    def get(self, request):
+        return super().get(request)
+
+    def get_context_data(self, **kwargs) -> dict:
+        return super().get_context_data(**kwargs)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'posts/detail.html'
